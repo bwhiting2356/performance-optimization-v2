5f10bf962ad6333084d12d707e1d59a1714aec08
Brendan Whiting <brendan.whiting@me.com>
2016-05-23 00:57:58 -0400

    got concat css working

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,34 @@
 'use strict';
 
-var gulp = require('gulp');
+var gulp = require('gulp'),
+  concat = require('gulp-concat');
 
-gulp.task("hello", function() {
-    console.log("Hello!")
+gulp.task("concatScripts", function() {
+    gulp.src([
+        'js/jquery.js', 
+        'js/fastclick.js', 
+        'js/foundation.js',
+        'js/foundation.equalizer.js',
+        'js/foundation.reveal.js'])
+    .pipe(concat("app.js"))
+    .pipe(gulp.dest("js"))
+});
+
+gulp.task("concatCSS", function() {
+    gulp.src([
+        'css/normalize.css',
+        'css/foundation.css',
+        'css/arvo.css',
+        'css/ubuntu.css',
+        'css/basics.css',
+        'css/menu.css',
+        'css/hero.css',
+        'css/photo-grid.css',
+        'css/modals.css',
+        'css/footer.css'
+        ])
+    .pipe(concat("styles.css"))
+    .pipe(gulp.dest("css"))
 })
 
 gulp.task("default", ["hello"], function() {
