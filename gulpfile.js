d0134d416057c418fb8f4dd8053a057e3679f3bd
Brendan Whiting <brendan.whiting@me.com>
2016-05-23 01:53:13 -0400

    deleted files and moved to other folders

--- gulpfile.js
+++ gulpfile.js
@@ -8,46 +8,46 @@ var gulp = require('gulp'),
 
 gulp.task("concatScripts", function() {
     return gulp.src([
-        'js/jquery.js', 
-        'js/fastclick.js', 
-        'js/foundation.js',
-        'js/foundation.equalizer.js',
-        'js/foundation.reveal.js'])
+        'src/js/jquery.js', 
+        'src/js/fastclick.js', 
+        'src/js/foundation.js',
+        'src/js/foundation.equalizer.js',
+        'src/js/foundation.reveal.js'])
     .pipe(concat("app.js"))
-    .pipe(gulp.dest("js"))
+    .pipe(gulp.dest("src/js"))
 });
 
 gulp.task("minifyScripts", ["concatScripts"], function() {
-    gulp.src("js/app.js")
+    gulp.src("src/js/app.js")
         .pipe(uglify())
         .pipe(rename('app.min.js'))
-        .pipe(gulp.dest('js'));
+        .pipe(gulp.dest('public/js'));
 });
 
 
 gulp.task("concatCSS", function() {
     return gulp.src([
-        'css/normalize.css',
-        'css/foundation.css',
-        'css/arvo.css',
-        'css/ubuntu.css',
-        'css/basics.css',
-        'css/menu.css',
-        'css/hero.css',
-        'css/photo-grid.css',
-        'css/modals.css',
-        'css/footer.css'
+        'src/css/normalize.css',
+        'src/css/foundation.css',
+        'src/css/arvo.css',
+        'src/css/ubuntu.css',
+        'src/css/basics.css',
+        'src/css/menu.css',
+        'src/css/hero.css',
+        'src/css/photo-grid.css',
+        'src/css/modals.css',
+        'src/css/footer.css'
         ])
     .pipe(concat("styles.css"))
-    .pipe(gulp.dest("css"))
+    .pipe(gulp.dest("src/css"))
 });
 
 
 gulp.task('minifyCSS', ['concatCSS'], function() {
-    gulp.src('css/styles.css')
+    gulp.src('src/css/styles.css')
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(rename('styles.min.css'))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('public/css'));
 });
 
 gulp.task("build", ['minifyScripts', 'minifyCSS']);
